ee2438018f5c0e9ce3f312dfd3573e6719b112cf
Zaid Afzal <zaidafzal@protonmail.com>
2021-08-17 09:24:00 +0500

    Update ProjectSecurity: remove django SECRET_KEY from code and store as environment variable

--- emumbaproject/settings.py
+++ emumbaproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qo63!cy!*i%8aw+k-fug*gj@@_9lh%gi))xhg1z7stft56&!tt'
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
+SECRET_KEY = get_random_secret_key() if not SECRET_KEY else SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
