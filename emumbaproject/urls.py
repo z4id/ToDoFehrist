52437daeb21074d0f3e33ad6fbf221e81fbe3664
Zaid Afzal <zaidafzal@protonmail.com>
2021-08-30 09:07:42 +0500

    Add Feature: add drf-yasg package to auto generate openapi based doc

--- emumbaproject/urls.py
+++ emumbaproject/urls.py
@@ -14,9 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# OpenAPI/Swagger based REST API Doc
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ToDoFehrist Restful API",
+        default_version='v1',
+        description="A ToDo App BackEnd built on top of Django-Python-PostgreSQL stack",
+        terms_of_service="https://www.emumba.com",
+        contact=openapi.Contact(email="zaid.afzal@emumba.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+
+    # Swagger API Doc Generator Views
+    re_path(r'^doc(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('', include('todofehrist.urls')),
 ]
