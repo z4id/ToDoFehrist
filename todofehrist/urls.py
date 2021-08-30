8af6f284935cb951aa17f320c7e13dd10bf6bd80
Zaid Afzal <zaidafzal@protonmail.com>
2021-08-30 09:27:32 +0500

    Add Feature: add reports views, urls and utility function to generate reports

--- todofehrist/urls.py
+++ todofehrist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 # from django.conf.urls import handler500
+from django.conf import settings
+from django.views.decorators.cache import cache_page
 
-from todofehrist.views import AppUserView, activate_account, AppUserLoginView
+from todofehrist.views import AppUserView, activate_account, AppUserLoginView, ReportView
 from todofehrist.exceptions import HTTPStatusCodeHandler
 
 urlpatterns = [
@@ -10,7 +12,10 @@ urlpatterns = [
     path('activate/<uid>/<token>', activate_account, name='activate'),
 
     # User Sign In
-    path('api/user/login', AppUserLoginView.as_view())
+    path('api/user/login', AppUserLoginView.as_view()),
+
+    # GET ?name= - Generate Report by Name
+    path('api/v1/reports/', cache_page(settings.REPORT_CACHE_TIME)(ReportView.as_view())),
 ]
 
 handler500 = HTTPStatusCodeHandler.handler500
