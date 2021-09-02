9c87d0286674dbed04f07ab9db9a1acbee1fbfdd
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-02 12:24:19 +0500

    Update Settings: update Environment Variables list and settings.py

--- emumbaproject/settings.py
+++ emumbaproject/settings.py
@@ -97,7 +97,7 @@ elif not SECRET_KEY:
     raise Exception("SECRET_KEY is not set in settings.py. "
                     "Check your Environment Variable 'SECRET_KEY'")
 
-DEBUG_PROPAGATE_EXCEPTIONS = False
+# DEBUG_PROPAGATE_EXCEPTIONS = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT_STAGE == EnvironmentStages.DEV.value:
@@ -107,9 +107,15 @@ else:
     DEBUG = False
     ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-ENV_ALLOWED_HOST = os.environ.get('ALLOWED_HOST', None)
-if ENV_ALLOWED_HOST:
-    ALLOWED_HOSTS = [ENV_ALLOWED_HOST]
+ENV_ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', None)
+if ENV_ALLOWED_HOSTS:
+    ALLOWED_HOSTS = [host.strip() for host in ENV_ALLOWED_HOSTS.split(',') if host]
+
+
+print(ALLOWED_HOSTS)
+
+
+GOOGLE_OAUTH_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_CLIENT_ID", '')
 
 # Application definition
 
@@ -122,9 +128,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
-    # 'oauth2_provider',
-    # 'social_django',
-    # 'rest_framework_social_oauth2',
     'todofehrist',
 ]
 
@@ -138,7 +141,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'todofehrist.middleware.LoggingRequestResponse',
 ]
-# 'social_django.middleware.SocialAuthExceptionMiddleware',
 
 ROOT_URLCONF = 'emumbaproject.urls'
 
@@ -295,47 +297,3 @@ REPORT_CACHE_TIME = 15*60  # seconds, 15 minutes
 
 CELERY_TIMEZONE = 'UTC'
 BROKER_URL = 'redis://127.0.0.1:6379'
-
-# Facebook configuration
-# SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('FACEBOOK_APP_ID', None)
-# SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('FACEBOOK_SECRET_KEY', None)
-# SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
-# # Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from facebook.
-# # Email is not sent by default, to get it, you must request the email permission:
-# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-# SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id, name, email' }
-# FACEBOOK_EXTENDED_PERMISSIONS = ['email']
-# SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email']
-# SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
-# SOCIAL_AUTH_PIPELINE = (
-#     'social_core.pipeline.social_auth.social_details',
-#     'social_core.pipeline.social_auth.social_uid',
-#     'social_core.pipeline.social_auth.auth_allowed',
-#     'social_core.pipeline.social_auth.social_user',
-#     'social_core.pipeline.user.get_username',
-#     'social_core.pipeline.social_auth.associate_by_email',
-#     'social_core.pipeline.user.create_user',
-#     'social_core.pipeline.social_auth.associate_user',
-#     'social_core.pipeline.social_auth.load_extra_data',
-#     'social_core.pipeline.user.user_details', )
-#
-# AUTHENTICATION_BACKENDS = (
-#
-#     # Facebook OAuth2
-#     'social_core.backends.facebook.FacebookAppOAuth2',
-#     'social_core.backends.facebook.FacebookOAuth2',
-#
-#     # django-rest-framework-social-oauth2
-#     'rest_framework_social_oauth2.backends.DjangoOAuth2',
-#
-#     # Django
-#     'django.contrib.auth.backends.ModelBackend',
-# )
-#
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         # OAuth
-#         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-#         'rest_framework_social_oauth2.authentication.SocialAuthentication',
-#     )
-# }
