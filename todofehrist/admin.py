866c9306f013e7fac733b1db5fe541fdf6d46f0d
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-02 15:44:04 +0500

    Delete Files: remove unintentionally added refactored files in dockerization of app (#24)

--- todofehrist/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from todofehrist.models import AppUser
-
-admin.site.register(AppUser)
--- todofehrist/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodofehristConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todofehrist'
