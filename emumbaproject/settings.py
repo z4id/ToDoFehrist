7124d9c30a57a9781cc2265d87bde9a55de1ff70
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-06 12:43:02 +0500

    Refactor middleware: move logging middleware from app to project level

--- emumbaproject/settings.py
+++ emumbaproject/settings.py
@@ -110,7 +110,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'todofehrist.middleware.LoggingRequestResponse',
+    'emumbaproject.middleware.LoggingRequestResponse',
 ]
 
 ROOT_URLCONF = 'emumbaproject.urls'
