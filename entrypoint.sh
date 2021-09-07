25ce55d1372e2db5bcd37c3bc364a8c5cb815af9
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-07 11:10:18 +0500

    update ENV variable names and python executable (#40)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "POSTGESQL" ]
-then
-    echo "Waiting for postgres..."
+echo "Waiting for postgres..."
 
     while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
-    echo "PostgreSQL started"
-fi
+echo "PostgreSQL started"
 
-#python3 manage.py flush --no-input
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py loaddata fixture_2
+#python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata fixture_2
 
 exec "$@"
