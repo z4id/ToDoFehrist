b9b3660dde67b7df78b967ff12988e1f586f58dc
Zaid Afzal <zaidafzal@protonmail.com>
2021-09-27 14:04:50 +0500

    Fix Kubernetes: fix ingress issues for deployment on minikube (#49)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
-
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
-
-echo "PostgreSQL started"
+#echo "Waiting for postgres..."
+#
+#    while ! nc -z $DB_HOST $DB_PORT; do
+#      sleep 0.1
+#    done
+#
+#echo "PostgreSQL started"
 
 #python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata fixture_2
+python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
